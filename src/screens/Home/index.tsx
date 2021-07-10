20e8d5bbc7d4ed76c5b1a5dee0f42e4deae207cc
fbsoares-lu <lsoareshfb@gmail.com>
2021-07-10 13:12:22 -0300

    Create a compoment of car

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -2,16 +2,30 @@ import React from 'react';
 import { StatusBar } from 'react-native';
 import { RFValue } from 'react-native-responsive-fontsize';
 
+import { Car } from '../../components/Car';
+
 import Logo from '../../assets/logo.svg';
 
 import {
     Container,
     Header,
     HeaderContent,
-    TotalCars
+    TotalCars,
+    CarList
 } from './styles';
 
 export function Home(){
+
+  const carData = {
+    brand: 'Porche',
+    name: 'Panamera',
+    rent: {
+      period: 'AO DIA',
+      price: 340,
+    },
+    thumbnail: 'https://www.pngkit.com/png/full/237-2375888_porsche-panamera-s.png'
+  }
+
   return(
     <Container>
       <StatusBar 
@@ -30,6 +44,12 @@ export function Home(){
           </TotalCars>
         </HeaderContent>
       </Header>
+
+      <CarList
+        data={[1,2,3,4,5,6,7,8]}
+        keyExtractor={item => String(item)}
+        renderItem={({ item }) => <Car data={carData}/>}
+      />
     </Container>
   );
 }
