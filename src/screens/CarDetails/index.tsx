783953cdce1a9cc81d6a8ce768ebc123e66d1f93
fbsoares-lu <lsoareshfb@gmail.com>
2021-07-11 12:46:38 -0300

    create a component carDetail

--- src/screens/CarDetails/index.tsx
+++ src/screens/CarDetails/index.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
 import { BackButton } from '../../components/BackButton';
+import { ImageSlider } from '../../components/ImageSlider';
 
 import {
     Container,
-    Header
+    Header,
+    CarImages,
+    Content,
+    Details,
+    Description,
+    Brand,
+    Name,
+    Rent,
+    Period,
+    Price,
+    About
 } from './styles';
 
 export function CarDetails(){
@@ -12,6 +23,29 @@ export function CarDetails(){
       <Header>
         <BackButton onPress={() => {}}/>
       </Header>
+
+      <CarImages>
+        <ImageSlider imagesUrl={['https://www.pngkit.com/png/full/237-2375888_porsche-panamera-s.png']} />
+      </CarImages>
+
+      <Content>
+        <Details>
+          <Description>
+            <Brand>Lamborghini</Brand>
+            <Name>Huracan</Name>
+          </Description>
+
+          <Rent>
+            <Period>Ao dia</Period>
+            <Price>R$ 580</Price>
+          </Rent>
+        </Details>
+
+        <About>
+          Este é automóvel desportivo. Surgiu do lendário touro de lide indultado
+          na praça Real Maestranza de Sevilla. É um belíssimo carro para quem gosta de acelerar.
+        </About>
+      </Content>
     </Container>
   );
 }
