62f3773d2630b62bf5639771a3ea956b47c8ebc3
fbsoares-lu <lsoareshfb@gmail.com>
2021-07-17 11:38:38 -0300

    Update modules of date

--- src/components/Calendar/index.tsx
+++ src/components/Calendar/index.tsx
@@ -1,19 +1,44 @@
 import React from 'react';
 import { Feather } from '@expo/vector-icons';
-import { Calendar as CustomCalendar, LocaleConfig} from 'react-native-calendars';
+import { 
+    Calendar as CustomCalendar, 
+    LocaleConfig,
+    DateCallbackHandler
+} from 'react-native-calendars';
 import { useTheme } from 'styled-components';
 
-LocaleConfig.locales['pt-br'] = {
-    monthNames: ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'],
-    monthNamesShort: ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez'],
-    dayNames: ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado'],
-    dayNamesShort: ['DOM', 'SEG', 'TER', 'QUA', 'QUI', 'SEX', 'SAB'],
-    today: 'Hoje'
-}
+import { ptBr } from './localeConfig';
+
+LocaleConfig.locales['pt-br'] = ptBr;
 
 LocaleConfig.defaultLocale = 'pt-br';
 
-export function Calendar(){
+interface MarkedDateProps {
+    [date: string]: {
+        color: string;
+        textColor: string;
+        disable?: boolean;
+        disableTouchEvent?: boolean;
+    }
+}
+
+interface CalendarProps {
+    markedDate: MarkedDateProps;
+    onDayPress: DateCallbackHandler;
+}
+
+interface DayProps {
+    dateString: string;
+    day: number;
+    month: number;
+    year: number;
+    timestamp: number;
+}
+
+function Calendar({
+    markedDate,
+    onDayPress
+}: CalendarProps){
     const theme = useTheme();
 
     return(
@@ -49,6 +74,15 @@ export function Calendar(){
             firstDay={1}
 
             minDate={new Date()}
+            markingType="period"
+            markedDates={markedDate}
+            onDayPress={onDayPress}
         />
     );
+}
+
+export {
+    Calendar,
+    MarkedDateProps,
+    DayProps
 }
--- src/screens/Scheduling/index.tsx
+++ src/screens/Scheduling/index.tsx
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Alert } from 'react-native';
 
 import { useTheme } from 'styled-components';
-import { useNavigation } from '@react-navigation/native';
+import { useNavigation, useRoute } from '@react-navigation/native';
 
 import { BackButton } from '../../components/BackButton';
 import { StatusBar } from 'react-native';
 import { Button } from '../../components/Button';
-import { Calendar } from '../../components/Calendar';
+import { Calendar, DayProps, MarkedDateProps } from '../../components/Calendar';
+import { generateInterval } from '../../components/Calendar/generateInterval';
+
+import { format } from 'date-fns';
+import { getPlataformDate } from '../../utils/getPlataformDate';
+import { CarDTO } from '../../dtos/CarDTO';
 
 import ArrowSvg from '../../assets/arrow.svg';
 
@@ -22,16 +28,61 @@ import {
     Footer
 } from './styles';
 
+
+interface RentalPeriod {
+    startFormatted: string;
+    endFormatted: string;
+}
+
+interface Params {
+    car: CarDTO;
+  }
+
 export function Scheduling(){
+    const [markedDates, setMarkedDates] = useState<MarkedDateProps>({} as MarkedDateProps);
+    const [lastSelectedDate, setLastSelectedDate] = useState<DayProps>({} as DayProps);
+    const [rentalPeriod, setRentalPeriod] = useState<RentalPeriod>({} as RentalPeriod);
+
     const theme = useTheme();
     const navigation = useNavigation();
+    const route = useRoute();
+    const { car } = route.params as Params;
 
     function handleConfirmRental() {
-        navigation.navigate('SchedulingDetails');
+        if (!rentalPeriod.startFormatted || !rentalPeriod.endFormatted) {
+            Alert.alert('Selecione o intervalo para alugar.');
+        } else {
+            navigation.navigate('SchedulingDetails', {
+                car,
+                dates: Object.keys(markedDates),
+            });
+        }
     }
 
     function handleBack() {
         navigation.goBack();
+    }
+
+    function handleChangeDate(date: DayProps) {
+        let start = !lastSelectedDate.timestamp ? date : lastSelectedDate;
+        let end = date;
+
+        if (start.timestamp > end.timestamp) {
+            start = end;
+            end = start;
+        }
+
+        setLastSelectedDate(end);
+        const interval = generateInterval(start, end);
+        setMarkedDates(interval);
+
+        const firstDate = Object.keys(interval)[0];
+        const endDate = Object.keys(interval)[Object.keys(interval).length -1];
+
+        setRentalPeriod({
+            startFormatted: format(getPlataformDate(new Date(firstDate)), 'dd/MM/yyyy'),
+            endFormatted: format(getPlataformDate(new Date(endDate)), 'dd/MM/yyyy'),
+        })
     }
 
     return(
@@ -55,18 +106,25 @@ export function Scheduling(){
                 <RentalPeriod>
                     <DateInfo>
                         <DateTitle>DE</DateTitle>
-                        <DateValue selected={false}></DateValue>
+                        <DateValue selected={!!rentalPeriod.startFormatted}>
+                            {rentalPeriod.startFormatted}
+                        </DateValue>
                     </DateInfo>
                     <ArrowSvg />
                     <DateInfo>
                         <DateTitle>ATÉ</DateTitle>
-                        <DateValue selected={false}></DateValue>
+                        <DateValue selected={!!rentalPeriod.endFormatted}>
+                            {rentalPeriod.endFormatted}
+                        </DateValue>
                     </DateInfo>
                 </RentalPeriod>
             </Header>
 
             <Content>
-                <Calendar/>
+                <Calendar
+                    markedDate={markedDates}
+                    onDayPress={handleChangeDate}
+                />
             </Content>
 
             <Footer>
